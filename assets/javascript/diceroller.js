df377f8af5a10ab5e4ea3aef9f74a86e75cd5fd3
Karina Vazquez <karina.vazquez6@yahoo.com>
2020-11-10 16:20:12 -0600

    final commit

--- assets/javascript/diceroller.js
+++ assets/javascript/diceroller.js
@@ -1,32 +1,26 @@
-let dieRollsArray = [];
-let roleDiceAmount = document.querySelector("#number-rolls");
-let reset = document.querySelector("#reset-button");
-let rollButton = document.querySelector("#roll-button");
-let totalnumber = document.querySelector("total");
-let showbutton = document.querySelector("show-all-rolls");
-let numberRolls = 20;
-let count = 0;
+let dieRolls = [];
+let rollButton = document.querySelector("#rollButton");
+let userInput = document.querySelector("#numOfRolls");
+let totalSpan = document.querySelector("#total");
+let showAllRolls = document.querySelector("#showAllRolls");
+let allRollsElement = document.querySelector("#allRollsDisplay");
 
-rollButton.addEventListener("click", function () {
-  //console.log("Nice Roll!");
-  let newRoleDiceAmount = Number(roleDiceAmount.innerHTML) + 1;
-  roleDiceAmount.innerHTML = newRoleDiceAmount;
-  if (Math.random() * 6 + 1) {
-    console.log("Amount Rolled!");
-    let newtotalnumberValue = Number(totalnumber.innerHTML) + 1;
-    totalnumber.innerHTML = newtotalnumberValue;
+rollButton.addEventListener("click", function (event) {
+  let diceCount = userInput.value;
+  let total = 0;
+  for (let i = 1; i < diceCount; i++) {
+    let roll = Math.floor(Math.random() * 6) + 1;
+    dieRolls.push(roll);
+    total = total + roll;
   }
-  dieRollsArray.push(roleDiceAmount);
+  totalSpan.innerHTML = total;
+  //console.log(dieRolls);
 });
-showbutton.addEventListener("click", function () {
-  const newDiceString = '<li class="dice1">' + numberRolled + "</li>";
-  allRollsElement.innerHTML += newDiceString;
-});
-reset.addEventListener("click", function () {
-  let newNumberofresetsValue = Number(numberofresets.innerHTML) + 1;
-  numberofresets.innerHTML = newNumberofresetsValue;
-  dice1.innerHTML = 0;
-  dice2.innerHTML = 0;
-  dice3.innerHTML = 0;
-  dice4.innerHTML = 0;
+
+showAllRolls.addEventListener("click", function (event) {
+  for (let i = 0; i < dieRolls.length; i = i + 1) {
+    const newDiceString = '<li class="dice">' + dieRolls[i] + "</li>";
+    allRollsElement.innerHTML = allRollsElement.innerHTML + newDiceString;
+    console.log(dieRolls[i]);
+  }
 });
